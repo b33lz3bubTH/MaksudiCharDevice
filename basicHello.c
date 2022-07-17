3d2bb676f9b8bd12c89b07902c80cab9a5a7c582
sourav_release_box <souravsunju@gmail.com>
2022-07-17 03:00:35 -0400

    first commit

---
+++ basicHello.c
@@ -0,0 +1,25 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+
+MODULE_LICENSE("GPL")
+MODULE_AUTHOR("Sourav A")
+MODULE_DESCPTION("Basic Hello World Kernel Program")
+MODULE_VERSION("1.0.0")
+
+
+static int __init helloworld_init(void) {
+	// return -EIO; // IO error
+	printk(KERN_INFO "Driver Init Successful\n");
+	return 0; // ALL OKAY
+}
+static void __exit helloworld_exit(void) {
+	printk(KERN_INFO "Driver Unmount Successful\n");
+}
+
+
+// Register initialization and exit function
+
+module_init(helloworld_init);
+module_exit(helloworld_exit);
